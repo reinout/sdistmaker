706bc5122219edfefd009512465cb4b8ac1a0e33
Reinout van Rees <reinout@vanrees.org>
2010-02-25 12:36:45 +0100

    Added optparse and made the logging more user-friendly

--- sdistmaker/iterator.py
+++ sdistmaker/iterator.py
@@ -5,12 +5,14 @@ A short hint on how it works is in the README.txt.
 import commands
 import imp
 import logging
+import optparse
 import os
 import sys
 
 from tha.tagfinder import extracter
 from tha.tagfinder import finder
 from tha.tagfinder import lister
+import pkg_resources
 
 from sdistmaker import maker
 
@@ -26,7 +28,6 @@ DEFAULTS = ['BASE',
             'BLACKLIST',
             'STOP_INDICATORS',
             'OUTDIR']
-
 
 logger = logging.getLogger('exporter')
 
@@ -44,33 +45,66 @@ def override_global_constants(defaults_file):
 
 
 def main(defaults_file=None, python=None):
-    """Main method, called by bin/sdists_from_tags
+    """Main method, called by bin/sdists_from_tags"""
+    usage = "Usage: %prog"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="Show debug output")
+    parser.add_option("-q", "--quiet",
+                      action="store_true", dest="quiet", default=False,
+                      help="Show minimal output")
+    parser.add_option("-d", "--defaults-file",
+                      dest="defaults_file", default=None,
+                      help="File with custom defaults")
+    parser.add_option("--print-example-defaults",
+                      action="store_true",
+                      dest="print_example_defaults", default=False,
+                      help="File with custom defaults")
+    (options, args) = parser.parse_args()
 
-    Start with -v to get INFO level logging, -vv for DEBUG level
-    logging.
-
-    """
-    level = logging.WARN
-    if '-v' in sys.argv[1:]:
-        level = logging.INFO
-    if '-vv' in sys.argv[1:]:
-        level = logging.DEBUG
-    logging.basicConfig(level=level,
+    if options.verbose:
+        log_level = logging.DEBUG
+    elif options.quiet:
+        log_level = logging.WARN
+    else:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level,
                         format="%(levelname)s: %(message)s")
+
+    if options.print_example_defaults:
+        print pkg_resources.resource_string('sdistmaker', 'defaults.py')
+        sys.exit(0)
+
     if defaults_file:
         override_global_constants(defaults_file)
+        logger.debug("Loaded defaults from %s", defaults_file)
+    if options.defaults_file:
+        override_global_constants(options.defaults_file)
+        logger.debug("Loaded defaults from %s", options.defaults_file)
 
     start = BASE
     rewrite_for_base = False
     if BASE_ON_SERVER:
+        logger.debug(
+            "Looking if the server base (%s) is usable instead of %s",
+            BASE_ON_SERVER, BASE)
         output = commands.getoutput("svn list %s" % BASE_ON_SERVER)
         if not 'Unable' in output:
             start = BASE_ON_SERVER
             rewrite_for_base = True
+        else:
+            logger.warn("BASE_ON_SERVER (%s) is not available: %s",
+                        BASE_ON_SERVER, output)
     startpoint = lister.SvnLister(start, ignore=BLACKLIST)
     info_extracter = extracter.BaseExtracter
     destination = os.path.abspath(OUTDIR)
+    if not os.path.exists(destination):
+        logger.warn("Destination dir %s does not exist, creating it",
+                    destination)
+        os.makedirs(destination)
 
+    logger.info("Looking for tags in %s", start)
     info = finder.Finder(startpoint, info_extracter,
                          stop_indicators=STOP_INDICATORS)
     for project in info.projects:
