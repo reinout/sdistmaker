5978d6ac527c67dabf32b31e4baee6f440613b6f
Reinout van Rees <reinout@vanrees.org>
2010-03-19 12:16:19 +0100

    Made Wouter's tarball detection more robust. Moved mocking stuff to the test_setup.py.

--- sdistmaker/tests/test_setup.py
+++ sdistmaker/tests/test_setup.py
@@ -1,10 +1,13 @@
+import commands
 import os
 import re
+import shutil
 import tempfile
 
 from zope.testing import renormalizing
 import z3c.testsetup
 
+import sdistmaker.maker
 
 checker = renormalizing.RENormalizing([
     (re.compile(r'\S+/pypi'), 'PYPI'),
@@ -13,15 +16,42 @@ checker = renormalizing.RENormalizing([
 
 
 def setup(test):
+    #Monkeypatching to prevent real action from taking place:
+    test.output_results = ['']
+
+    def mock_getstatusoutput(cmd):
+        print "Command:", cmd
+        return 0, test.output_results.pop(0)
+
+    test.orig_getstatusoutput = commands.getstatusoutput
+    commands.getstatusoutput = mock_getstatusoutput
+
+    def mock_copy(src, dest):
+        print "Mock copy %s -> %s" % (src, dest)
+        open(dest, 'w').write('mock')
+
+    test.orig_copy = shutil.copy
+    shutil.copy = mock_copy
+
+    def mock_find_tarball(dist_dir, name, version):
+        return name + '-' + version + '.tar.gz'
+
+    test.orig_find_tarball = sdistmaker.maker.find_tarball
+    sdistmaker.maker.find_tarball = mock_find_tarball
+
     test.tempdir = tempfile.mkdtemp()
     test.pypidir = os.path.join(test.tempdir, 'pypi')
     os.mkdir(test.pypidir)
     test.globs.update({'pypidir': test.pypidir,
+                       'output_results': test.output_results,
                        })
 
 
 def teardown(test):
-    pass
+    # Restore originals:
+    commands.getstatusoutput = test.orig_getstatusoutput
+    shutil.copy = test.orig_copy
+    sdistmaker.maker.find_tarball = test.orig_find_tarball
 
 
 test_suite = z3c.testsetup.register_all_tests(
